1cbdb913dab1c59b0c988169c1eba535478b0207
Ke Thien <giakethien@gmail.com>
2019-03-11 14:56:17 +0100

    Edit about me component

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -4,18 +4,18 @@
     <div class="about__content">
       <div class="about__item--left">
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi corrupti dolore deserunt aut nesciunt saepe
-          natus iusto nemo, provident enim sunt, aperiam dolorum atque in sit illum, cumque ipsum quis eaque vero
-        </p><p>
-          suscipit hic nisi optio. Voluptatem quis veniam architecto! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Iure, porro?
+          Hi, my name is Ke Thien Nguyen. <br>I live in Brussels, Belgium.  I'm an alumnus from Le&nbsp;Wagon Brussels Bootcamp and Cepegra Front&#8209;end Developer. There, I learned to code and worked in a team. 
+        </p>
+        <p>
+          <em>“Learn, Create and Share”</em> is my motto in life.  I'm mostly self&#8209;taught (thank you, Internet). <br>
+          I'm passionate about creating things with my hands like sculpting, folding origami, printing 3d objects. 
         </p>
         <a href="https://www.linkedin.com/in/kethien/"><img src="~assets/img/Logo-2CRev-14px.png" alt=""></a>
         <a href="https://drive.google.com/file/d/1DkvP9AMbDi5GRBB-eUZoWXtK6uRNXMis/view?usp=sharing">My resume</a>
       </div>
       <div class="about__item--right">
         <div class="about__img">
-          <img src="~assets/img/meAt10PC.jpg" alt="me at 10">
+          <img src="~assets/img/origami-dragon-hand.jpg" alt="me at 10">
         </div>
       </div>
     </div>
