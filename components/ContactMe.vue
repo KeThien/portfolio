e3ec6bba241c92ba2dc7af408a877e36758dd9f8
Ke Thien <giakethien@gmail.com>
2019-03-12 11:23:20 +0100

    contact form animation thankyou and ready

--- components/ContactMe.vue
+++ components/ContactMe.vue
@@ -1,44 +1,142 @@
 <template>
   <section id="hp-contact" class="contact">
     <h2>Contact</h2>
-    <form name="contact" action="/thank-you" netlify-honeypot="bot-field" method="post" netlify>
-      <input type="hidden" name="form-name" value="contact">
-      <p class="hidden">     
-        <label>Don’t fill this out: <input name="bot-field"></label>   
-      </p>
-      <div class="form-container">
-        <p>
-          If you want to say <em>’Hello’</em> or hire me
+    <transition name="fly-away">
+      <form
+        v-show="!isSend"
+        name="contact"
+        action
+        netlify-honeypot="bot-field"
+        method="post"
+        netlify
+        @submit.prevent="sendForm"
+      >
+        <input type="hidden" name="form-name" value="contact">
+        <p class="hidden">
+          <label>
+            Don’t fill this out:
+            <input name="bot-field">
+          </label>
         </p>
-        
-        <div class="form-wrap">
-          <div class="form__left">
-            <label for="name">Name*</label>
-            <input id="contact-name" type="text" name="name" placeholder="Your name">
-            <label for="email">Email*</label>
-            <input id="contact-email" type="email" name="email" placeholder="Your email">
-            <label for="subject">Subject</label>
-            <input id="contact-subject" type="text" name="subject" placeholder="The subject">
-          </div>
-          
-          <div class="form__right">
-            <label for="msg">Message</label>
-            <textarea id="contact-msg" class="common" name="msg" placeholder="Write something..." />
-          </div>
-        </div>
+        <div class="form-container">
+          <p>
+            If you want to say
+            <em>’Hello’</em> or hire me
+          </p>
 
-        <div class="contact-notice">
-          *required
+          <div class="form-wrap">
+            <div class="form__left">
+              <label for="name">Name*</label>
+              <input id="contact-name" type="text" name="name" placeholder="Your name">
+              <label for="email">Email*</label>
+              <input id="contact-email" type="email" name="email" placeholder="Your email">
+              <label for="subject">Subject</label>
+              <input id="contact-subject" type="text" name="subject" placeholder="The subject">
+            </div>
+
+            <div class="form__right">
+              <label for="msg">Message</label>
+              <textarea id="contact-msg" class="common" name="msg" placeholder="Write something..." />
+            </div>
+          </div>
+
+          <div class="contact-notice">
+            *required
+          </div>
+          <button type="submit" class="btn-submit">
+            <i class="fa fa-paper-plane" /> Send
+          </button>
         </div>
-        <input type="submit" value="Submit">
+      </form>
+    </transition>
+    <transition name="fade">
+      <div v-if="showThank" class="thank-you" :style="{ height: formH + 'px', width: formW + 'px' }">
+        <h3>
+          Thank you
+        </h3>
+        <p>
+          Your message has been sent.
+        </p>
+        <button @click="closeThankYou">
+          <i class="fa fa-times" /> Close
+        </button>
       </div>
-    </form>
+    </transition>
   </section>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      isSend: false,
+      showThank: false,
+      formW: Number,
+      formH: Number
+    }
+  },
+  methods: {
+    sendForm() {
+      const form = document.querySelector('form')
+      this.formW = form.offsetWidth
+      this.formH = form.offsetHeight
+      this.isSend = !this.isSend
+      this.showThankYou()
+    },
+    showThankYou() {
+      window.setTimeout(() => {
+        this.showThank = true
+      }, 500)
+    },
+    closeThankYou() {
+      this.showThank = false
+      window.setTimeout(() => {
+        this.isSend = false
+      }, 500)
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+.fly-away-enter-active,
+.fly-away-leave-active {
+  transition: all 0.5s;
+}
+.fly-away-enter {
+  opacity: 0;
+}
+.fly-away-leave-to {
+  transform: translateX(200%);
+  opacity: 0;
+}
+.thank-you {
+  border: 0px solid white;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h3 {
+    font-family: 'Aleo', serif;
+    font-size: 2em;
+    font-weight: 300;
+    text-transform: uppercase;
+  }
+  button {
+    margin: 30px;
+    padding: 9px;
+    background-color: #fff3e3;
+    border: none;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+}
 </style>
