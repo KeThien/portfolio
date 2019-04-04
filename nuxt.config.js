08949eeab1f5c12cb3e5bd6264be1943032736da
Ke Thien <giakethien@gmail.com>
2019-04-04 18:28:18 +0200

    generate: true

--- nuxt.config.js
+++ nuxt.config.js
@@ -130,7 +130,7 @@ module.exports = {
     hostname: 'https://kethien.be',
     cacheTime: 1000 * 60 * 15,
     gzip: true,
-    generate: false, // Enable me when using nuxt generate
+    generate: true, // Enable me when using nuxt generate
     routes: [
       {
         url: '/works/',
